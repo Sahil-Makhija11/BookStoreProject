781d09ce5df57b441e1a55c5af8a10fc1236157c
Sahil-Makhija11 <sahilmakhija700@gmail.com>
2025-06-16 10:59:45 +0500

    worked on backed, connected server and mongodb through mongoose to save the data locally

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+import bookRoute from './route/book.route.js';
+const app = express();
+app.use(cors());
+
+dotenv.config();
+
+const PORT= process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+//connect to mongoDB
+try {
+    mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, 
+    });
+    console.log('Connected to MongoDB');
+} catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+}
+
+//Defining Routes
+app.use("/book", bookRoute);
+
+app.listen(PORT, () => {                
+  console.log('Example app listening on port ', PORT);
+});
